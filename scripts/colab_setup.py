eb5051dda80d23db34c4cc5c18cda9486a6020f9
Emediong Francis <emediongfrancis@Benditos-MacBook-Pro.local>
2025-08-15 19:21:40 +0100

    Revert indentation

--- scripts/colab_setup.py
+++ scripts/colab_setup.py
@@ -122,7 +122,8 @@ print("Discord and Vencord installed successfully!")
 !mkdir -p src/userplugins/notifyServerJoins
 
 # Create the plugin file
-plugin_code = '''/*
+plugin_code = '''
+/*
  * Vencord, a Discord client mod
  * Copyright (c) 2025 Emediong Francis
  * SPDX-License-Identifier: GPL-3.0-or-later
@@ -193,7 +194,8 @@ export default definePlugin({
         console.log("Unsubscribing from GUILD_MEMBER_ADD event via FluxDispatcher.");
         FluxDispatcher.unsubscribe("GUILD_MEMBER_ADD", onMemberJoin);
     }
-});'''
+});
+'''
 
 with open('src/userplugins/notifyServerJoins/index.ts', 'w') as f:
     f.write(plugin_code)
